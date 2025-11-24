93014b91b9bf2ab369da7767856bb6e0ea800522
Marian505 <75570468+Marian505@users.noreply.github.com>
2025-11-24 12:31:48 +0100

    add load dotenv

--- agent.py
+++ agent.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
 
 agent = create_agent(
     model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.0)
