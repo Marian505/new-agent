8d0c26a2d7c4df31a550085975eb8cbcce258afc
Marian505 <75570468+Marian505@users.noreply.github.com>
2025-12-17 11:02:21 +0100

    make basic agent async and update tests

--- src/basic_agent.py
+++ src/basic_agent.py
@@ -1,15 +1,22 @@
 from dotenv import load_dotenv
-from langchain.agents import create_agent
+from langchain.agents import create_agent, AgentState
 from langchain_tavily import TavilySearch
+from langchain_anthropic import ChatAnthropic
 
 load_dotenv()
+
+fast_model=ChatAnthropic(model="claude-haiku-4-5-20251001", temperature=0.0)
 
 search = TavilySearch(max_results=3)
 
 system_prompt = "You are a helpful assistant with web search tool."
 
+class CustomState(AgentState):
+    pass
+
 agent = create_agent(
-    model = "claude-sonnet-4-5-20250929",
+    model=fast_model,
     tools=[search],
-    system_prompt=system_prompt
+    system_prompt=system_prompt,
+    state_schema=CustomState
 )
--- tests/integration_tests/test_basic_agent.py
+++ tests/integration_tests/test_basic_agent.py
@@ -1,48 +1,54 @@
-from basic_agent import agent
+import pytest
+
 from langchain.messages import HumanMessage
-from rich.pretty import pprint
 from agentevals.trajectory.llm import create_trajectory_llm_as_judge, TRAJECTORY_ACCURACY_PROMPT
 from langgraph.checkpoint.memory import InMemorySaver
 
+from basic_agent import agent
 
-def test_basic_agent():
-    result = agent.invoke({"messages": [HumanMessage("What is Java?")]})
+@pytest.mark.asyncio
+async def test_basic_agent():
+    result = await agent.ainvoke({"messages": [HumanMessage("What is Java? Do not use search tool.")]})
 
     assert result["messages"] is not None
     tool_massages = [message for message in result["messages"] if message.__class__.__name__ == "ToolMessage"]
     assert len(tool_massages) == 0
 
-def test_basic_agent_web_search():
-    result = agent.invoke({"messages": [HumanMessage("What is black hole? Search on web.")]})
+@pytest.mark.asyncio
+async def test_basic_agent_web_search():
+    result = await agent.ainvoke({"messages": [HumanMessage("What is black hole? Search on web.")]})
     
     assert result["messages"] is not None
     tool_massage = next(message for message in result["messages"] if message.__class__.__name__ == "ToolMessage")
     assert tool_massage is not None
     assert tool_massage.name == "tavily_search"
 
-def test_trajectory_quality():
+@pytest.mark.asyncio
+async def test_trajectory_quality():
     evaluator = create_trajectory_llm_as_judge(  
         model="claude-haiku-4-5-20251001",
         prompt=TRAJECTORY_ACCURACY_PROMPT,  
     )  
     
-    result = agent.invoke({"messages": [HumanMessage("What is black hole? Search on web.")]})
+    result = await agent.ainvoke({"messages": [HumanMessage("What is black hole? Search on web.")]})
 
     evaluation = evaluator(outputs=result["messages"])
     assert evaluation["score"] is True
 
-def test_basic_agent_no_mem(): 
-    result = agent.invoke({"messages": [HumanMessage("My name is Majo?")]})
-    result2 = agent.invoke({"messages": [HumanMessage("What is my name?")]})
+@pytest.mark.asyncio
+async def test_basic_agent_no_mem(): 
+    _result = await agent.ainvoke({"messages": [HumanMessage("My name is Majo?")]})
+    result2 = await agent.ainvoke({"messages": [HumanMessage("What is my name?")]})
 
     last_msg = result2["messages"][-1]
     assert "majo" not in last_msg.content.lower(), "Agent should not remember name."
 
-def test_basic_agent_mem():
+@pytest.mark.asyncio
+async def test_basic_agent_mem():
     config = {"configurable": {"thread_id": "thread_id_1"}}
     agent.checkpointer = InMemorySaver()
-    result = agent.invoke({"messages": [HumanMessage("My name is Majo?")]}, config=config)
-    result2 = agent.invoke({"messages": [HumanMessage("What is my name?")]}, config=config)
+    _result = await agent.ainvoke({"messages": [HumanMessage("My name is Majo?")]}, config=config)
+    result2 = await agent.ainvoke({"messages": [HumanMessage("What is my name?")]}, config=config)
 
     last_msg = result2["messages"][-1]
     assert "majo" in last_msg.content.lower(), "Agent should remember name."
